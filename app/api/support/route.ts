c9ca8194d01fb5fdca9a6247de2b5c74d49d19fc
Cabin <sdn.warehouse@gmail.com>
2024-12-20 16:55:30 +0700

    logo center

--- app/api/support/route.ts
+++ app/api/support/route.ts
@@ -82,8 +82,8 @@ export async function POST(request: NextRequest) {
       </head>
       <body>
         <div class="container">
-         <div class="header flex items-center justify-center py-5 border-b-2 border-[#f58220]">
-            <img src="cid:logo" alt="SDN Thailand Logo" class="w-[100px] h-auto">
+         <div class="header flex items-center justify-center">
+          <img src="cid:logo" alt="SDN Thailand Logo" class="logo">
           </div>
           <div class="content">
             <h1 style="color: #f58220;">คำขอสื่อรณรงค์ใหม่</h1>
